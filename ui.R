2defc21036bca80f6a30f0dcc66ae101bbc52350
BJRJ <123.biju@gmail.com>
2017-09-16 18:09:43 -0700

    Add files via upload

--- ui.R
+++ ui.R
@@ -6,16 +6,18 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-    textInput("stocks","Enter Stock Symbols Separated by Comma"), sliderInput("days","Enter the number of days",value = "",
+    textInput("stocks","Enter Stock Symbols Separated by Comma",value="GOOGL,AMZN"), sliderInput("days","Enter the number of days",value = 20,
                                                                                min=1,max=49),
     checkboxInput("somevalue", "Log scale for stock price axis?",FALSE),
-    checkboxInput("featureplot", "Generate Feature Plot?", FALSE),
-    checkboxInput("corrplot", "Generate Correlation Plot?", FALSE),
+    checkboxInput("featureplot", "Generate Paired X-Y Plot?", TRUE),
+    checkboxInput("corrplot", "Generate Paired Correlation Plot?", TRUE),
     
     
       submitButton(text="Go!")
       
     ),
+    
+    
     mainPanel(
     plotlyOutput("plot"),plotOutput("plot2"),plotOutput("plot3")
     )
